510886af736f4322b5dccd2d1187e69099945923
Pierre Tran <pierretranva@gmail.com>
2022-09-23 21:26:43 -0400

    feat: added function that will return simulated total cost

--- primalDualAlg/Slot.py
+++ primalDualAlg/Slot.py
@@ -1,24 +1,27 @@
 class Slot:
     def __init__(self, state: int):
-        self.download = None
+        self.download: int = 0
         self.state = state
         self.z = None
         self.y = 0
 
     def set_z(self, value: int):
         self.z = value
-    
-    def get_z(self):
-        return self.z
-    
+
+    def set_y(self, value: int):
+        self.y = value
+
     def set_state(self, value: int):
-        self.state =value
+        self.state = value
 
     def set_download(self, value: int):
         self.download = value
 
-    def set_y(self, value: int):
-        self.y =value
+    def get_y(self):
+        return self.y
+
+    def get_z(self):
+        return self.z
 
     def get_state(self):
         return self.state
--- primalDualAlg/primal.py
+++ primalDualAlg/primal.py
@@ -1,10 +1,18 @@
-import Slot
+from Slot import Slot
 
-def print_slot_contents(data: list):
-    sum_d: int = 0
+import random
+
+
+def total_cost(data: list, c: int):
+    cost: int = 0
     for slot in data:
-        sum_d += slot.get_download()
-    return sum_d
+        if slot.get_download() >= 1:
+            cost += c
+        elif random.random() <= slot.get_download():
+            cost += c
+
+    return cost
+
 
 def is_tight(c: int, data, currIndex: int, list_size: int):
     result = 0
@@ -13,6 +21,7 @@ def is_tight(c: int, data, currIndex: int, list_size: int):
             result += data[j].get_y()
         result *= data[currIndex].get_state()
     return result <= c
+
 
 def primalDual(data: list):
     # d(t)
@@ -27,43 +36,53 @@ def primalDual(data: list):
     theta = ((1 + 1 / c) ** c) - 1
 
     currIndex = 0
-    if data[currIndex].get_state() == 1:
-        for i in range(0, currIndex):
-            download_value = 0
-            for n in range(i, currIndex):
-                download_value += data[n].get_download()
-            if download_value < 1:
-                
-                # z(t) = 1 - download_sum
-                data[currIndex].set_z(1 - download_value)
+    while currIndex < len(data):
+        if data[currIndex].get_state() == 1:
+            for i in range(0, currIndex):
+                download_value = 0
+                for n in range(i, currIndex):
+                    download_value += data[n].get_download()
+                if download_value < 1:
 
-                data[currIndex].set_download(
-                    data[currIndex].get_download()
-                    + (1 / c) * download_value
-                    + (1 / (theta + c))
-                )
-                data[currIndex].set_y(1)
-    else:  # state is not equal to 1
-        for i in currIndex:
-            if i < currIndex:
-                # z(t) = z(t-1)
-                data[i].set_z(data[i - 1].get_z())
-            else:
-                # z(t)=1
-                data[i].set_z(1)
-    for i in range(len(data)):
-        if data[currIndex].get_state() == 0:
-            for j in range(1, currIndex):
-                # if y "is not tight"
-                if not is_tight(c, data, currIndex, len(currIndex)):
+                    # z(t) = 1 - download_sum
+                    data[currIndex].set_z(1 - download_value)
+
+                    data[currIndex].set_download(
+                        data[currIndex].get_download()
+                        + (1 / c) * download_value
+                        + (1 / (theta + c))
+                    )
                     data[currIndex].set_y(1)
-    return data
-    
+        else:  # state is not equal to 1
+            for i in range(currIndex):
+                if i < currIndex:
+                    # z(t) = z(t-1)
+                    data[i].set_z(data[i - 1].get_z())
+                else:
+                    # z(t)=1
+                    data[i].set_z(1)
+        for i in range(len(data)):
+            if data[currIndex].get_state() == 0:
+                for j in range(currIndex):
+                    # if y "is not tight"
+                    if not is_tight(c, data, currIndex, len(data)):
+                        data[currIndex].set_y(1)
+        currIndex += 1
+    return data, c
+
+
 def main():
-    values = [1, 1, 0, 1, 1, 0, 0, 1, 0]
+    values = [1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1]
+    # for i in range(40):
+    #     values.append(random.randint(0, 2))
+
     list_of_data = []
     for i in values:
         newSlot = Slot(i)
         list_of_data.append(newSlot)
-    list_of_data = primalDual(list_of_data)
-    print_slot_contents(list_of_data)
+    list_of_data, c = primalDual(list_of_data)
+    print(total_cost(list_of_data, c))
+
+
+if __name__ == "__main__":
+    main()
