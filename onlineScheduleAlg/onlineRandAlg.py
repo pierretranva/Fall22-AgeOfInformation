b2c75aeb0462b8fae2278bd85c8a14da348be1b3
AndrewTran03 <andrewtranva@gmail.com>
2022-10-01 00:29:17 -0400

    feat: fixed the end of primal-dual algo to not be updated for every slot in while-loop (suggested by Mr. Zhongdong), fixed summation of d(t) in online-random scheduling algo, cleaned up variables in both programs, and added more detailed comments in online-random scheduling algo

--- onlineScheduleAlg/onlineRandAlg.py
+++ onlineScheduleAlg/onlineRandAlg.py
@@ -1,6 +1,5 @@
 from tkinter import W
-from Slot import Slot
-
+from Slot import *
 import random
 
 
@@ -11,55 +10,62 @@ def total_cost(data: list, c: int):
             cost += c
         elif random.random() <= slot.get_download():
             cost += c
-
     return cost
 
 
-def online(data: list):
-    # cost value
+def online_rand_alg(data: list):
+    
     c = 2
     # constant theta
     theta = ((1 + 1 / c) ** c) - 1
-    # random number between [0,1)
+    # random number between [0,1) (so a double in the range of 0 and 1 exclusive)
     u = random.random()
-    currIndex = 0
-    while currIndex < len(data):
-        if data[currIndex].get_state() == 1:
-            for i in range(0, currIndex + 1):
-                download_value = 0
-                for j in range(0, currIndex + 1):
-                    download_value += data[j].get_download()
 
-                if download_value < 1:
-                    sumationValue = 0
-                    for o in range(i, currIndex + 1):
-                        sumationValue += data[o].get_download() + (1 / (theta * c))
+    currIndex = 0
+    # Iterates through each Slot in the list (from 1 to T inclusive)
+    while currIndex < len(data):
+        # state s(t) is equal to 1
+        if data[currIndex].get_state() == 1: 
+            for i in range(0, currIndex + 1):
+                download_sum = 0
+                # Gets the summation of d(t) from 1 to t (inclusive)
+                for j in range(i, currIndex + 1):
+                    download_sum += data[j].get_download()
+
+                if download_sum < 1:
+                    # d(t) = d(t) + (download_sum / c) + (1 / theta * c)
                     data[currIndex].set_download(
-                        data[currIndex].get_download() + (sumationValue / c)
+                        data[currIndex].get_download() + (download_sum / c) + (1 / (theta * c))
                     )
+        # d(t) [Presum] = d(t) [Currsum]
         data[currIndex].set_presum(data[currIndex].get_d_sum())
+        # d(t) [Currsum] = d(t) [Currsum] + min(d(t), 1)
         data[currIndex].set_d_sum(
             data[currIndex].get_d_sum() + min(data[currIndex].get_download(), 1)
         )
-
+        # if u is in-between the range of d(t) [Presum] and d(t) [Currsum]
         if data[currIndex].get_presum() <= u < data[currIndex].get_d_sum():
             u += 1
-        else:
-            pass
+        else: 
+            # Denoting an "idle"/"passed" state
+            pass 
         currIndex += 1
     return data, c
 
 
 def main():
     values = [0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0]
-    # for i in range(40):
-    #     values.append(random.randint(0, 2))
+    '''
+    Used for Temporary Testing (Generating Random Values):
+    for i in range(40):
+        values.append(random.randint(0, 2))
+    '''
 
     list_of_data = []
     for i in values:
         newSlot = Slot(i)
         list_of_data.append(newSlot)
-    list_of_data, c = online(list_of_data)
+    list_of_data, c = online_rand_alg(list_of_data)
     print(total_cost(list_of_data, c))
 
 
--- primalDualAlg/primal.py
+++ primalDualAlg/primal.py
@@ -1,7 +1,5 @@
-from Slot import Slot
-
+from Slot import *
 import random
-
 
 def total_cost(data: list, c: int):
     cost: int = 0
@@ -10,7 +8,6 @@ def total_cost(data: list, c: int):
             cost += c
         elif random.random() <= slot.get_download():
             cost += c
-
     return cost
 
 
@@ -23,28 +20,30 @@ def is_tight(c: int, data, currIndex: int, list_size: int):
     return result <= c
 
 
-def primalDual(data: list):
+def primal_dual_alg(data: list):
 
+    # cost value
     c = 2
     # constant theta
     theta = ((1 + 1 / c) ** c) - 1
 
     currIndex = 0
+    # Iterates through each Slot in the list (from 1 to T inclusive)
     while currIndex < len(data):
-        if data[currIndex].get_state() == 1:
+        if data[currIndex].get_state() == 1: # state s(t) is equal to 1
             for i in range(0, currIndex):
-                download_value = 0
-                for n in range(i, currIndex):
-                    download_value += data[n].get_download()
-                if download_value < 1:
-
+                download_sum = 0
+                # Gets the summation of d(t) from 1 to t (inclusive)
+                for n in range(i, currIndex + 1):
+                    download_sum += data[n].get_download()
+                if download_sum < 1:
                     # z(t) = 1 - download_sum
-                    data[currIndex].set_z(1 - download_value)
-
+                    data[currIndex].set_z(1 - download_sum)
+                    # d(t) = d(t) + (download_sum / c) + (1 / theta * c)
                     data[currIndex].set_download(
                         data[currIndex].get_download()
-                        + (1 / c) * download_value
-                        + (1 / (theta + c))
+                        + (1 / c) * download_sum
+                        + (1 / (theta * c))
                     )
                     data[currIndex].set_y(1)
         else:  # state is not equal to 1
@@ -53,28 +52,33 @@ def primalDual(data: list):
                     # z(t) = z(t-1)
                     data[i].set_z(data[i - 1].get_z())
                 else:
-                    # z(t)=1
+                    # z(t) = 1
                     data[i].set_z(1)
-        for i in range(len(data)):
-            if data[currIndex].get_state() == 0:
-                for j in range(currIndex):
-                    # if y "is not tight"
-                    if not is_tight(c, data, currIndex, len(data)):
-                        data[currIndex].set_y(1)
         currIndex += 1
+    # Correction: Variable y(t) when s(t) = 0 gets updated below at the end of slot T
+    for i in range(len(data)):
+        if data[currIndex].get_state() == 0: # state s(t) is equal to 0
+            for j in range(currIndex):
+                # if y "is not tight"
+                if not is_tight(c, data, currIndex, len(data)):
+                    data[currIndex].set_y(1)
+        
     return data, c
 
 
 def main():
     values = [0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0]
-    # for i in range(40):
-    #     values.append(random.randint(0, 2))
+    '''
+    Used for Temporary Testing (Generating Random Values):
+    for i in range(40):
+        values.append(random.randint(0, 2))
+    '''
 
     list_of_data = []
     for i in values:
         newSlot = Slot(i)
         list_of_data.append(newSlot)
-    list_of_data, c = primalDual(list_of_data)
+    list_of_data, c = primal_dual_alg(list_of_data)
     print(total_cost(list_of_data, c))
 
 
