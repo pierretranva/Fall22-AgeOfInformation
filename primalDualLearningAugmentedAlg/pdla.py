d7ccf95bf02823a9a269003fb00e2df342b4d0e1
AndrewTran03 <andrewtranva@gmail.com>
2022-10-29 13:23:46 -0400

    feat: Added the Input Values Needed for the PDLA Function

--- primalDualLearningAugmentedAlg/pdla.py
+++ primalDualLearningAugmentedAlg/pdla.py
@@ -1,5 +1,7 @@
 from primalDualLearningAugmentedAlg.Package import *
 import random
+from scipy.stats import poisson
+from scipy.stats import lomax
 
 def total_cost(data: list, c: int):
     cost: int = 0
@@ -19,7 +21,7 @@ def is_tight(c: int, data, currIndex: int, list_size: int):
         result *= data[currIndex].get_state()
     return result <= c
 
-def primal_dual_learning_augmentation_alg(data: list):
+def primal_dual_learning_augmentation_alg(lambda: float, data: list, alpha: list, d: Int):
     lambda_values = []
     alpha_values = [[],[],[]]
     x: int = 0
@@ -36,22 +38,27 @@ def primal_dual_learning_augmentation_alg(data: list):
     return None
 
 def main():
-    lambda_values = [0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0]
-    '''
-    Used for Temporary Testing (Generating Random Values):
-    for i in range(40):
-        values.append(random.randint(0, 2))
-    '''
-    '''
-    list_of_data = []
-    for i in values:
-        newSlot = Slot(i)
-        list_of_data.append(newSlot)
-    list_of_data, c = primal_dual_alg(list_of_data)
-    print(total_cost(list_of_data, c))
-    '''
-    pass
+    lambda_values = [0.4, 0.6, 0.8, 1.0]
+    distribution = ["Poisson", "Pareto"]
+    selected_distribution = distribution[0]
+    mean = 1
+    D = 0 # Holds the distribution used in the PDLA
+    if (selected_distribution == "Poisson"):
+        D = poisson(mean)
+    elif (selected_distribution == "Pareto"):
+        pass # Will figure out later
     
+    A_values = []
+    for i in range(1, 1001):
+        prob = random.uniform(0, 1)
+        A_value.append(prob)
+
+    d = 100 # Represents that we will split every second into 100 time units
+    
+    primal_dual_learning_augmentation_alg(lambda_values[random.randint(0, len(lambda_values)], D , A_values, d)
+
+    return None
+
 
 if __name__ == "__main__":
     main()
